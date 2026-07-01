6b79b53ee7326a62276d95a7cd6469b7c1abe14a
Fatmata06 <nfatmata614@gmail.com>
2026-07-01 06:53:39 +0000

    feat: 5. Ajouter categorie en lui ajoutant un produit

--- imperative.php
+++ imperative.php
@@ -124,6 +124,68 @@ $code=readline("Entrer le code: ");
         }
     }
 
+    //5 Ajouter categorie en lui ajoutant un produit, demande confiirmation , lors de lajout produit
+
+
+          $codeIsValid = true;
+    
+   do { 
+        
+        $code = readline("saisir le code :");
+        if (empty($code)) {
+            echo "le code est obligatoire \n";
+             $codeIsValid = false;
+        }else{
+            foreach ($categories as  $categorie ) {
+               if (($categorie["code"]) === $code) {
+                $codeIsValid = false;
+                echo "le code existe deja ...\n"; 
+         }
+       }  
+}
+        
+
+
+    } while (!$codeIsValid);
+    
+     $nomIsValid = true;
+  do { 
+        
+        $nom = readline("saisir le nom : ");
+        if (empty($nom)) {
+            echo "le nom est obligatoire";
+             $nomIsValid= false;
+        }else{
+            foreach ($categories as  $categorie ) {
+               if (($categorie["nom"]) === $nom) {
+                $nomIsValid = false;
+                echo "le nom existe deja ..."; 
+         }
+       }  
+}
+    } while (!$nomIsValid);
+
+     $produits = [];
+     do {
+         $produit =   [
+                    "nom" => readline("saisir le nom : "),
+                    "reference" => readline("saisir la reference : "),
+                    "prix" => (int)readline("saisir le prix : "),
+                    "quantite" => (int)readline("saisir la quantité : ")
+                  ];
+          $produits[]= $produit;
+
+          $choix = strtolower(readline(" voulez vous continuer  oui/non "));
+          
+     } while ($choix === "oui");
+
+    $categorie  =   [
+            "code" => $code,
+            "nom" => $nom,
+            "produits" =>  $produits 
+         ];
+
+         $categories[] = $categorie;
 
 
 
